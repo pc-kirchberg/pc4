5066c689c0e6f1adb56254dc5175d81b5f1d4aa6
Marks Polakovs <github@markspolakovs.me>
2019-07-30 12:36:28 +0300

    Add Netlify CMS

--- config.rb
+++ config.rb
@@ -13,6 +13,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# Blog
+activate :blog
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
